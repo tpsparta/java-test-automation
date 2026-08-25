99a97bfdca7bd1382d8e81a8a04e2483caa0bf98
t <tprior@spartaglobal.com>
2026-08-25 18:44:04 +0100

    Fixed code and tests

--- src/main/java/com/sparta/tfd/FilmClassification.java
+++ src/main/java/com/sparta/tfd/FilmClassification.java
@@ -7,9 +7,13 @@ public class FilmClassification {
         String result;
         if (ageOfViewer < 12)
         {
-            result = "U PG & 12 films are available.";
+            result = "U & PG films are available.";
         }
         else if (ageOfViewer < 15)
+        {
+            result = "U PG & 12 films are available.";
+        }
+        else if (ageOfViewer < 18)
         {
             result = "U PG 12 & 15 films are available.";
         }
--- src/test/java/com/sparta/tfd/FilmClassificationTests.java
+++ src/test/java/com/sparta/tfd/FilmClassificationTests.java
@@ -10,10 +10,12 @@ public class FilmClassificationTests {
     @ParameterizedTest
     @DisplayName("Film classification tests")
     @CsvSource({
-            "11, U PG & 12 films are available.",
-            "12, U PG 12 & 15 films are available.",
-            "14, U PG 12 & 15 films are available.",
-            "15, All films are available."
+            "11, U & PG films are available.",
+            "12, U PG & 12 films are available.",
+            "14, U PG & 12 films are available.",
+            "15, U PG 12 & 15 films are available.",
+            "17, U PG 12 & 15 films are available.",
+            "18, All films are available."
     })
     public void fc_test_1(int age, String expectedRes)
     {
